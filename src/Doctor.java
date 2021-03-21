7be17388247a7207c262bb1d69f91572123b366c
cristianiniguez <felizpc1@gmail.com>
2021-03-21 18:11:51 -0400

    definir clases y sus componentes: metodo constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("Building Doctor object");
+    }
+
+    Doctor(String name) {
+        System.out.println("Doctor's assigned name is " + name);
+    }
+
     public void showName() {
         System.out.println(name);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Cristian Iñiguez"); // Doctor's assigned name is Cristian Iñiguez
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName(); // Alejandro Rodriguez
     }
