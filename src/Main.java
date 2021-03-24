d18a2d22544ca9af188c0d14eb956b43faba0b40
cristianiniguez <felizpc1@gmail.com>
2021-03-24 19:07:34 -0400

    definir clases y sus componentes: getters y setters

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,13 @@ public class Main {
         System.out.println(myDoctor.speciality); // Odontology
 
         Patient patient = new Patient("Alejandra", "alejandra@gmail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight()); // 54.6 Kg.
+
+        patient.setPhoneNumber("123456789"); // El número telefónico debe ser máximo de 8 dígitos
+        System.out.println(patient.getPhoneNumber()); // null
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber()); // 12345678
     }
 }
