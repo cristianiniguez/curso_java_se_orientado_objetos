39ce6f72319752df33177d78f83c7dca3b6a53a2
cristianiniguez <felizpc1@gmail.com>
2021-03-28 19:20:49 -0400

    ensamblando objetos al proyecto: definiendo las citas disponibles

--- src/ui/UIDoctorMenu.java
+++ src/ui/UIDoctorMenu.java
@@ -1,8 +1,13 @@
 package ui;
 
+import model.Doctor;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UIDoctorMenu {
+    public static ArrayList<Doctor> doctorsWithAvailableAppointments = new ArrayList<>();
+
     public static void showDoctorMenu() {
         int response = 0;
         do {
@@ -52,12 +57,31 @@ public class UIDoctorMenu {
                 System.out.println("1. Correct");
                 System.out.println("2. Incorrect");
 
+                int responseDate = Integer.valueOf(sc.nextLine());
+                if (responseDate == 2) continue;
 
+                int responseTime = 0;
+                String time;
+                do {
+                    System.out.println("Insert the time available for date: " + date + " [16:00]");
+                    time = sc.nextLine();
+                    System.out.println("Your time is: " + time);
+                    System.out.println("1. Correct");
+                    System.out.println("2. Incorrect");
+                    responseTime = Integer.valueOf(sc.nextLine());
+                } while (responseTime == 2);
 
-
+                UIMenu.doctorLogged.addAvailableAppointment(date, time);
+                checkDoctorAvailableAppointments(UIMenu.doctorLogged);
             } else if (response == 0) {
                 showDoctorMenu();
             }
         } while (response != 0);
+    }
+
+    private static void checkDoctorAvailableAppointments(Doctor doctor) {
+        if (doctor.getAvailableAppointments().size() > 0 && !doctorsWithAvailableAppointments.contains(doctor)) {
+            doctorsWithAvailableAppointments.add(doctor);
+        }
     }
 }
